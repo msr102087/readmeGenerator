5d520a91745358e65e73ff88a4a6c763dfe10dae
Megan Russell <megan.russell50@gmail.com>
2021-02-23 15:49:57 -0800

    adding in user input questions

--- index.js
+++ index.js
@@ -30,9 +30,75 @@ function promptUser(){
         {
 
             type: 'input',
-            name: ''
+            name: 'installation',
+            message: 'Describe how to install this application.',
+
+        },
+
+        {
+
+            type: 'input',
+            name: 'usage',
+            message: 'Please describe how to use this application.',
+
+        },
+
+        {
+
+            type: 'list',
+            name: 'license',
+            message: 'Select the appropriate license for this project.',
+            choices: [
+                'Apache',
+                'Academic',
+                'GNU',
+                'ISC',
+                'MIT',
+                'Mozilla',
+                'Open'
+                
+            ]
+
+
+        },
+
+        {
+
+            type: 'input',
+            name: 'contributors',
+            message: 'Please list the contributors of this project.',
+
+
+        },
+
+        {
+
+            type: 'input',
+            name: 'tests',
+            message: 'Are there any tests associated with this project?',
+
+
+        },
+
+        {
+
+            type: 'input',
+            name: 'username',
+            message: 'Please enter your GitHub username.',
+
+        },
+
+        {
+
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email address.',
+
         }
-    ])
+
+
+    ]);
 }
 
 
+init();
